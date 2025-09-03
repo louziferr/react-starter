ec6b371897bc4cdf38016164c5823351c7e9ee49
luzieahrens <luzie.ahrens@gmail.com>
2025-09-03 11:20:54 +0200

    add pictures

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,10 +1,26 @@
 import ShopItem from "@/components/shop-item";
 
 export default function ShopPage() {
+  const items = [
+    { name: "Shoes", price: 79.99 },
+    { name: "Shirt", price: 29.99 },
+    { name: "Pants", price: 49.99 },
+    { name: "Hoodie", price: 69.99 },
+    { name: "Bottle", price: 9.99 },
+    { name: "Cap", price: 19.99 },
+  ];
+
   return (
     <>
-      <h1 className="text-[200px] text-white modak">Shop.</h1>
-      <ShopItem />
+      <h1 className="text-[100px] lg:text-[200px] text-white modak text-center">
+        Shop.
+      </h1>
+
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 mb-10">
+        {items.map((item, index) => (
+          <ShopItem title={item.name} price={item.price} key={index} />
+        ))}
+      </div>
     </>
   );
 }
--- components/shop-item.tsx
+++ components/shop-item.tsx
@@ -1,9 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
-export default function ShopItem() {
+interface ShopItemProps {
+  title: string;
+  price: number;
+}
+
+export default function ShopItem({ title, price }: ShopItemProps) {
   return (
     <>
       <div className="bg-white p-4 rounded-xl border-2 border-black w-80">
-        <h2 className="text-xl text-gray-600">Shop Item</h2>
+        <h2 className="text-xl text-gray-600">{title}</h2>
         <div className="flex justify-center">
           <img
             className="my-8 w-60 h-60"
@@ -11,7 +16,7 @@ export default function ShopItem() {
             alt="ShopItem"
           ></img>
         </div>
-        <h2 className="text-xl text-gray-600 text-right">79.99 EUR</h2>
+        <h2 className="text-xl text-gray-600 text-right">{price} EUR</h2>
 
         <div className="flex justify-center">
           <button className="mt-4 bg-sky-700 text-white py-1 px-14 rounded-xl border-2 border-black">
