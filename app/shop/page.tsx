08aa4efbeec9d0a1ceb27a7119229bd1f7d05ac1
luzieahrens <luzie.ahrens@gmail.com>
2025-09-03 10:28:00 +0200

    style shop item

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -3,7 +3,7 @@ import ShopItem from "@/components/shop-item";
 export default function ShopPage() {
   return (
     <>
-      <h2 className="text-[200px] text-white modak">Shop.</h2>
+      <h1 className="text-[200px] text-white modak">Shop.</h1>
       <ShopItem />
     </>
   );
--- components/shop-item.tsx
+++ components/shop-item.tsx
@@ -1,7 +1,24 @@
+/* eslint-disable @next/next/no-img-element */
 export default function ShopItem() {
   return (
     <>
-      <h3>Shop Item</h3>
+      <div className="bg-white p-4 rounded-xl border-2 border-black w-80">
+        <h2 className="text-xl text-gray-600">Shop Item</h2>
+        <div className="flex justify-center">
+          <img
+            className="my-8 w-60 h-60"
+            src="shopping-store.svg"
+            alt="ShopItem"
+          ></img>
+        </div>
+        <h2 className="text-xl text-gray-600 text-right">79.99 EUR</h2>
+
+        <div className="flex justify-center">
+          <button className="mt-4 bg-sky-700 text-white py-1 px-14 rounded-xl border-2 border-black">
+            Buy
+          </button>
+        </div>
+      </div>
     </>
   );
 }
