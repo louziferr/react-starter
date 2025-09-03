5f6b6c715a55de86239ce712e9f7a92eb23a530b
luzieahrens <luzie.ahrens@gmail.com>
2025-09-03 11:36:38 +0200

    add total price calculation

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,14 +1,19 @@
+"use client";
+
 import ShopItem from "@/components/shop-item";
+import { useState } from "react";
 
 export default function ShopPage() {
   const items = [
-    { name: "Shoes", price: 79.99 },
-    { name: "Shirt", price: 29.99 },
-    { name: "Pants", price: 49.99 },
-    { name: "Hoodie", price: 69.99 },
-    { name: "Bottle", price: 9.99 },
-    { name: "Cap", price: 19.99 },
+    { name: "Shoes", price: 79.99, image: "shoes.svg" },
+    { name: "Shirt", price: 29.99, image: "shirt.svg" },
+    { name: "Pants", price: 49.99, image: "pants.svg" },
+    { name: "Hoodie", price: 69.99, image: "hoodie.svg" },
+    { name: "Bottle", price: 9.99, image: "bottle.svg" },
+    { name: "Cap", price: 19.99, image: "cap.svg" },
   ];
+
+  const [total, setTotal] = useState(0);
 
   return (
     <>
@@ -18,8 +23,19 @@ export default function ShopPage() {
 
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 mb-10">
         {items.map((item, index) => (
-          <ShopItem title={item.name} price={item.price} key={index} />
+          <ShopItem
+            title={item.name}
+            price={item.price}
+            image={item.image}
+            total={total}
+            setTotal={setTotal}
+            key={index}
+          />
         ))}
+      </div>
+
+      <div className="flex justify-center">
+        <p className="text-3xl mb-10 text-white">Total: {total} EUR</p>
       </div>
     </>
   );
--- components/shop-item.tsx
+++ components/shop-item.tsx
@@ -2,24 +2,36 @@
 interface ShopItemProps {
   title: string;
   price: number;
+  image: string;
+  total: number;
+  setTotal: (total: number) => void;
 }
 
-export default function ShopItem({ title, price }: ShopItemProps) {
+export default function ShopItem({
+  title,
+  price,
+  image,
+  total,
+  setTotal,
+}: ShopItemProps) {
+  function buy() {
+    setTotal(total + price);
+  }
+
   return (
     <>
       <div className="bg-white p-4 rounded-xl border-2 border-black w-80">
         <h2 className="text-xl text-gray-600">{title}</h2>
         <div className="flex justify-center">
-          <img
-            className="my-8 w-60 h-60"
-            src="shopping-store.svg"
-            alt="ShopItem"
-          ></img>
+          <img className="my-8 w-60 h-60" src={image} alt="ShopItem"></img>
         </div>
         <h2 className="text-xl text-gray-600 text-right">{price} EUR</h2>
 
         <div className="flex justify-center">
-          <button className="mt-4 bg-sky-700 text-white py-1 px-14 rounded-xl border-2 border-black">
+          <button
+            className="mt-4 bg-sky-700 text-white py-1 px-14 rounded-xl border-2 border-black"
+            onClick={buy}
+          >
             Buy
           </button>
         </div>
