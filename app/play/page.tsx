bf06dbcb984b8d08da5098e07f13fc1866d01255
luzieahrens <luzie.ahrens@gmail.com>
2025-09-04 10:12:51 +0200

    start implementing memory game

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -3,7 +3,7 @@ import MemoryGame from "@/components/memory-game";
 export default function PlayPage() {
   return (
     <>
-      <h1 className="text-[100px] text-white modak">Play.</h1>
+      <h1 className="text-[100px] text-white modak text-center">Play.</h1>
       <MemoryGame />
     </>
   );
--- components/memory-game.tsx
+++ components/memory-game.tsx
@@ -1,7 +1,48 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
+
+import { useEffect, useState } from "react";
+
+interface Card {
+  id: number;
+  img: number;
+}
+
 export default function MemoryGame() {
+  // create array of cards
+  const [cards, setCards] = useState<Card[]>([]);
+
+  useEffect(() => {
+    const cards: Card[] = [];
+    // fill array
+    for (let i = 1; i <= 24; i++) {
+      cards.push({
+        id: i, // id for card (numbers 1-24)
+        img: (i % 12) + 1, // img references (numbers 1-12, twice)
+      });
+    }
+    // shuffle array
+    cards.sort(() => Math.random() - 0.5);
+    setCards(cards);
+  }, []); // useEffect runs once when the component is rendered
+
   return (
     <div className="bg-white p-4 rounded">
       <h2 className="text-center text-rose-700 font-bold">Memory Game</h2>
+      <div className="grid grid-cols-6 m-4 gap-4">
+        {cards.map((item, index) => (
+          <div
+            key={index}
+            className="w-12 h-12 border-2 border-sky-700 rounded cursor-pointer"
+          >
+            <img
+              className="hidden"
+              src={`memory/${item.img}.svg`}
+              alt={`Card-Item`}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
